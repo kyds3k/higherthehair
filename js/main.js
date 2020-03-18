c79a8d9fc84d066dd7c1256dee8209af67c5e124
Adam Darby <kyds3k@gmail.com>
2020-03-18 12:57:14 +0300

    Commit before uploading to Porkbun

---
+++ js/main.js
@@ -0,0 +1,22 @@
+const inputField = document.querySelector('input[type="text"]')
+
+document.querySelector('form').addEventListener('submit', event => {
+    event.preventDefault()
+    let searchTerm = removePunctuation(inputField.value).toLowerCase();
+    $('.bar').fadeOut();
+
+
+    if(($('.' + searchTerm)).length) {
+        console.log('found that shit');
+        $('.' + searchTerm).fadeIn();
+    } else {
+        $('.notfound').fadeIn();
+    }
+    
+})
+
+let regex = /[/\s/g\t!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~]/g;
+
+function removePunctuation(string) {
+    return string.replace(regex, '');
+  }
