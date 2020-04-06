1f94741dc0537ea54aa5b68952d3511a598b9ebd
Adam Darby <adam.darby@fortyfour.com>
2020-04-05 23:22:37 -0400

    Whoops! Way too long since I committed. About to clone for Higher The Hair. Maybe.

--- js/main.js
+++ js/main.js
@@ -1,53 +1,61 @@
-$('#bars').flexdatalist({
+$("#bars").flexdatalist({
   searchContain: true,
   searchByWord: true
 });
 
-const inputField = document.querySelector('input[type="text"]')
+const inputField = document.querySelector('input[type="text"]');
 
-document.querySelector('form').addEventListener('submit', event => {
-  event.preventDefault()
+document.querySelector("form").addEventListener("submit", event => {
+  event.preventDefault();
   let searchTerm = removePunctuation(inputField.value).toLowerCase();
   console.log(searchTerm);
-  $('.bar').stop(true, true).fadeOut();
+  $(".bar")
+    .stop(true, true)
+    .fadeOut();
 
-
-  if (($('.bar[data-name=' + searchTerm + ']')).length) {
-    $('.bar[data-name=' + searchTerm + ']').stop(true, true).delay(500).fadeIn();
+  if ($(".bar[data-name=" + searchTerm + "]").length) {
+    $(".bar[data-name=" + searchTerm + "]")
+      .stop(true, true)
+      .delay(500)
+      .fadeIn();
   } else {
-    $('.notfound').stop(true, true).delay(500).fadeIn();
+    $(".notfound")
+      .stop(true, true)
+      .delay(500)
+      .fadeIn();
   }
 
-  $('#bars').val('');
-  $('.show-all').fadeIn();
+  $("#bars").val("");
+  $(".show-all").fadeIn();
+});
 
-})
-
-$('.show-all').click(function () {
-  $('.bar').not('.notfound').fadeIn();
+$(".show-all").click(function() {
+  $(".bar")
+    .not(".notfound")
+    .fadeIn();
   $(this).fadeOut();
   return false;
 });
 
-$('.bar a').click(function () {
-  let clickedURL = $(this).attr('href');
+$(".bar a").click(function() {
+  let clickedURL = $(this).attr("href");
   trackOutboundLink(clickedURL);
 });
 
-let punctuation = '!"#$%&\'()*+,-./:;<=>?@[\\]^_`{|}~';
+let punctuation = "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~";
 
-let regex = new RegExp('[' + punctuation + ']', 'g');
+let regex = new RegExp("[" + punctuation + "]", "g");
 
 function removePunctuation(string) {
-  string = string.replace(/\s+/g, '');
-  return string.replace(regex, '');
+  string = string.replace(/\s+/g, "");
+  return string.replace(regex, "");
 }
 
-let trackOutboundLink = function (url) {
-  ga('send', 'event', 'outbound', 'click', url, {
-    'transport': 'beacon',
-    'hitCallback': function () {
+let trackOutboundLink = function(url) {
+  ga("send", "event", "outbound", "click", url, {
+    transport: "beacon",
+    hitCallback: function() {
       document.location = href;
     }
   });
-}
+};
